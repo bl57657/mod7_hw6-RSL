7c2c4b5a4fe98221e0f555aaa48d053c4d30f551
Tysen Bair <tysenbair@mail.weber.edu>
2017-02-28 17:18:23 +0000

    initial

---
+++ Real_Salt_Lake_HW6_task1.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Real_Salt_Lake_HW6_task1.c
+ *       	Usage:  ./Real_Salt_Lake_HW6_task1.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/28/2017 05:17:09 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Tysen Bair (), tysenbair@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
